95ef197d8439a2a23e7f447cb42d7256ea43dd21
Gaoura <Gaoura@outlook.com>
2022-07-03 01:54:56 +0200

    Add sheet factory

---
+++ CSV Transformer/Assets/Code/Codebase/SheetFactory.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+
+namespace CSVTransformer.Codebase
+{
+    public sealed class SheetFactory
+    {
+        private RowFactory RowFactory { get; set; } = new();
+
+        public Sheet Build(List<string> csv_lines)
+        {
+            var sheet = new Sheet();
+            foreach (var csv_line in csv_lines)
+            {
+                var row = RowFactory.Build(csv_line);
+                sheet.Add(row);
+            }
+
+            return sheet;
+        }
+    }
+}
---
+++ CSV Transformer/Assets/Code/Tests/SheetFactoryTest.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using CSVTransformer.Codebase;
+
+namespace CSVTransformer.Tests.Unit
+{
+    public sealed class SheetFactoryTest
+    {
+        [Test]
+        public void Build_ShouldReturnTheRightCountOfRowsInSheet_WhenCSVLinesAreProvided()
+        {
+            var factory = new SheetFactory();
+            var csv_lines = new List<string>()
+            {
+                "field00,field01",
+                "field10,field11",
+                "field20,field21",
+                "field30,field31"
+            };
+            var sheet = factory.Build(csv_lines);
+
+            Assert.That
+            (
+                sheet.RowCount,
+                Is.EqualTo(4)
+            );
+        }
+    }
+}
